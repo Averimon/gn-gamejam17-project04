d6c21598d7d3ff952a75e78069e633f9fd7d95de
Jonas Pfisterer <jonas.pfisterer@student.tugraz.at>
2025-11-15 13:15:58 +0200

    Started work on theCustomer behavour

--- Assets/Scripts/TableUnit.cs
+++ Assets/Scripts/TableUnit.cs
@@ -35,6 +35,10 @@ public class TableUnit : MonoBehaviour
     private void HandleItemPlaced(Consumable item)
     {
         itemOnTable = item;
+        foreach (Customer customer in _customers)
+        {
+            customer.ReceiveItem(itemOnTable);
+        }
     }
 
     public Vector3 GetSeat(Customer customer)
